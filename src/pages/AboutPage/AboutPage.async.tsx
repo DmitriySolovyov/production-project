680e4a9726fdc7f24cd2624f4227862c6985ce4b
DmitriySolovyov <solovyov1986@mail.ru>
2024-06-27 13:14:14 +0300

    initial commit

---
+++ src/pages/AboutPage/AboutPage.async.tsx
@@ -0,0 +1,13 @@
+import {lazy} from "react";
+
+// @ts-ignore
+export const AboutPageAsync = lazy(() => new Promise(resolve => {
+    // @ts-ignore
+    setTimeout(() => resolve(import('./AboutPage')), 1500)
+}));
+
+
+
+
+
+
